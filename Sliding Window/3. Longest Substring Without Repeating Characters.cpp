51da3ddf08333816a7f73eae63f8fcba94ca8ff8
Shubham Gupta <46106892+shubham1908@users.noreply.github.com>
2020-07-09 09:58:34 +0530

    Create 3. Longest Substring Without Repeating Characters.cpp

---
+++ Sliding Window/3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+    
+    int lengthOfLongestSubstring(string s) {
+        int ans=0,j=0;
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.length();i++){
+            if(mp.find(s[i])!=mp.end()){
+                j = max(mp[s[i]]+1,j);
+            }
+            mp[s[i]]=i;
+            ans = max(ans, i-j+1);
+        }
+        return ans;
+    }
+};
