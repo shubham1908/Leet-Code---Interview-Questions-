1ef453acb7a598e4ddb93dab8181f78fbfe156cd
Shubham Gupta <46106892+shubham1908@users.noreply.github.com>
2020-07-21 13:05:54 +0530

    Medium

--- Array/238. Product of Array Except Self.cpp
+++ Array/238. Product of Array Except Self.cpp
@@ -9,7 +9,7 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int>ans(nums.size(),1);
         for(int i=1;i<nums.size();i++){
-            ans[i]=ans[i-1]*nums[i-1];
+            ans[i] = ans[i-1] * nums[i-1];
         }
         int x=1;
         for(int i=nums.size()-1;i>=0;i--){
