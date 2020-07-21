6855969349242309c20a3814a81f3daaa794f21b
Shubham Gupta <46106892+shubham1908@users.noreply.github.com>
2020-07-21 12:59:27 +0530

    Easy

--- Linked-List/203. Remove Linked List Elements.cpp
+++ Linked-List/203. Remove Linked List Elements.cpp
@@ -9,15 +9,15 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        ListNode* ahead=new ListNode(-1);
-        ahead->next=head;
-        ListNode* temp=ahead;
-        while(temp!=NULL){
-            if(temp->next!=NULL && temp->next->val==val){
-                temp->next=temp->next->next;
+        ListNode* ahead = new ListNode(-1);
+        ahead->next = head;
+        ListNode* temp = ahead;
+        while(temp != NULL){
+            if(temp->next != NULL && temp->next->val == val){
+                temp->next = temp->next->next;
             }
             else{
-                temp=temp->next;
+                temp = temp->next;
             }
         }
         return ahead->next;
